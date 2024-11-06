6fda87d82fd57bb256f77726d03887d3b29bd962
Caleb Skinner <calebskinner01@gmail.com>
2024-11-06 13:53:40 -0600

    update

--- Data Manipulation.R
+++ Data Manipulation.R
@@ -1,10 +1,49 @@
 library("hoopR")
+library("tidyverse")
+library("Rfast")
 
 # sleeper point computation
-source("SleeperBayesian/Scrape.R")
+source("Scrape.R")
 
-# current year
-df_2025 <- load_nba_player_box(seasons = 2025)
+# fix box score into our desired format
+clean_box_score <- function(df){
+  df %>% clean_names() %>%
+    clean_names() %>%
+    filter(season_type == 2) %>%
+    select(game_date, athlete_display_name, points, three_point_field_goals_made,
+           rebounds, assists, steals, turnovers, blocks) %>%
+    rename(
+      "date" = game_date,
+      "pts" = points,
+      "ast" = assists,
+      "stl" = steals,
+      "x3p" = three_point_field_goals_made,
+      "trb" = rebounds,
+      "blk" = blocks,
+      "tov" = turnovers,
+      "name" = athlete_display_name)
+}
 
 # last year
-df_2024 <- load_nba_player_box(seasons = 2024)
+df_2024 <- load_nba_player_box(seasons = 2024) %>%
+  clean_box_score() %>%
+  sleeper_points()
+
+# current year
+df_2025 <- load_nba_player_box(seasons = 2025) %>%
+  clean_box_score() %>%
+  sleeper_points()
+
+# select nine players ie Daniel's team for funsies
+team <- c("Kyrie Irving", "Jalen Suggs", "Miles Bridges", "DeMar DeRozan", "Alperen Sengun",
+          "Draymond Green", "Deandre Ayton", "Jordan Poole", "Collin Sexton")
+
+df_2024 %>%
+  filter(name %in% team) %>%
+  group_by(name) %>%
+  summarize(
+    n_last_year = 
+  )
+
+
+
